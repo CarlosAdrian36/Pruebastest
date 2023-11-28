df16ee2226ff4666564fefb49b8a89bccc7a6a47
Carlos.Arroyo <carlos23_adrian@hotmail.com>
2023-11-28 10:23:05 -0600

    TEST Suites

---
+++ Test Suites/testdelogin.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testdelogin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9c0da82b-de39-458d-8eed-2184959f7fb3</testSuiteGuid>
+   <testCaseLink>
+      <guid>617323d2-42ce-4e29-824a-7c61de5dd54d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TESTLOGIN</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
